2d84fecbc7647b07a9015807f67d0cff674eae3b
nicolashenrique-dev <nicolashenrique2410@gmail.com>
2026-07-29 21:45:22 -0300

    Initial commit

---
+++ scanner.py
@@ -0,0 +1,46 @@
+import socket 
+from concurrent.futures import ThreadPoolExecutor
+import ipaddress
+
+ip_alvo = input('IP do alvo: ')
+ip_teste = ip_alvo
+portas = range(1,1025)
+
+def Scan(ip_alvo, porta):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1.0)
+    result = s.connect_ex((ip_alvo, porta))
+    if result == 0:
+        try:
+            banner = s.recv(1024).decode().strip()
+            print(f"Porta {porta} aberta | {banner}")
+        except:
+            print(f"Porta {porta} aberta")
+
+    s.close()
+    
+    
+
+
+def ip_valido (ip_teste):
+    try:
+        ipaddress.IPv4Address(ip_teste)
+        return True
+    except ValueError:
+        return False
+
+
+if ip_valido(ip_teste) == False:
+    try:
+        ip_alvo = socket.gethostbyname(ip_alvo)
+        for porta in portas:
+                Scan(ip_alvo, porta)
+                
+    except socket.gaierror:
+        print('Host inválido')
+        exit()
+
+else: 
+    for porta in portas:
+        Scan(ip_alvo, porta)
+   
