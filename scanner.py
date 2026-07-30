afaa7adbeb605daf8c17db05282395fb1d94ad85
nicolashenrique-dev <nicolashenrique2410@gmail.com>
2026-07-29 21:52:49 -0300

    Corrige indentação e atualiza scanner.py

--- scanner.py
+++ scanner.py
@@ -5,6 +5,7 @@ import ipaddress
 ip_alvo = input('IP do alvo: ')
 ip_teste = ip_alvo
 portas = range(1,1025)
+open("resultado.txt", "w").close()
 
 def Scan(ip_alvo, porta):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,6 +15,8 @@ def Scan(ip_alvo, porta):
         try:
             banner = s.recv(1024).decode().strip()
             print(f"Porta {porta} aberta | {banner}")
+            with open("resultado.txt", "a") as f:
+    f.write(f"Porta {porta} aberta | {banner}\n")
         except:
             print(f"Porta {porta} aberta")
 
